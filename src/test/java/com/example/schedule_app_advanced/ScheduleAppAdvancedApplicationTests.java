315de0aa3a806b6b4df451f281fdaeb2d983c5da
Gyum1 <xoruadl123@gmail.com>
2025-05-23 16:37:39 +0900

    chore: 프로젝트 초기 생성 및 설정 파일 추가

---
+++ src/test/java/com/example/schedule_app_advanced/ScheduleAppAdvancedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedule_app_advanced;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleAppAdvancedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
