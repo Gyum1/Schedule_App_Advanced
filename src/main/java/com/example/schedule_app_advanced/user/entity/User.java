ac73cf1ca936f4ef86c4184fa52a3aad5d36735e
Gyum1 <xoruadl123@gmail.com>
2025-05-29 11:16:26 +0900

    feat: User 엔티티 생성 및 JPA Auditing 적용

---
+++ src/main/java/com/example/schedule_app_advanced/user/entity/User.java
@@ -0,0 +1,34 @@
+package com.example.schedule_app_advanced.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+}
